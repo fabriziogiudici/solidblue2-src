3bec77345994cc87df905bed120f0b44e4864f6d
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2015-12-04 20:43:25 +0100

    Fixed headers.

--- modules/Main/src/main/java/it/tidalwave/integritychecker2/Storage.java
+++ modules/Main/src/main/java/it/tidalwave/integritychecker2/Storage.java
@@ -35,7 +35,7 @@ import java.util.stream.Stream;
  *
  * The persistent storage for the fingerprint data.
  *
- * @author  Fabrizio Giudici <Fabrizio dot Giudici at tidalwave dot it>
+ * @author  Fabrizio Giudici (Fabrizio.Giudici@tidalwave.it)
  * @version $Id: Class.java,v 631568052e17 2013/02/19 15:45:02 fabrizio $
  *
  **********************************************************************************************************************/
